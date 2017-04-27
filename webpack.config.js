0b9a9305e6ad7013998f858dd4dbd47860646fe0
Mauro Velazquez <maurovelazquez1@gmail.com>
2017-04-27 20:55:36 -0300

    Finish kinda

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,46 @@
-var path = require('path');
-var webpack = require('webpack');
+const { resolve } = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './index.js',
+  entry: [
+    'react-hot-loader/patch',
+
+    'webpack-dev-server/client?http://localhost:8080',
+
+    'webpack/hot/only-dev-server',
+
+    './index.js',
+  ],
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    hot: true,
+    contentBase: resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+
   module: {
-    loaders: [
+    rules: [
       {
         test: /.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        }
+        use: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader?modules' ]
       }
     ]
-  }
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  ]
 }
